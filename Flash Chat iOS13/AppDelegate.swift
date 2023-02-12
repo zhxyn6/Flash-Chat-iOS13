dce165966756bdcb483671a8a73a508fda294a31
Xinran <zhxyn6@gmail.com>
2023-02-12 16:44:08 -0500

    successfully install and build pod library firebase

--- Flash Chat iOS13/AppDelegate.swift
+++ Flash Chat iOS13/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // Use Firebase library to configure APIs
+        FirebaseApp.configure()
+        
         return true
     }
 
--- Podfile
+++ Podfile
@@ -1,8 +1,15 @@
 # Uncomment the next line to define a global platform for your project
 platform :ios, '13.0'
 
+use_frameworks! :linkage => :static
+
 target 'Flash Chat iOS13' do
   # Pods for Flash Chat iOS13
   pod 'CLTypingLabel' , '~> 0.4.0'
 
+  # Add the pods for the Firebase products you want to use in your app
+  # For example, to use Firebase Authentication and Cloud Firestore
+  pod 'FirebaseAuth'
+  pod 'FirebaseFirestore'
+  
 end
